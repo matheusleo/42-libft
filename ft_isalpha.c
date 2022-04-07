f045553bef1c76f5906d16c1288e8de2e987293d
matheusleo <matheusleotrab@gmail.com>
2022-04-07 20:13:32 -0300

    modify the function ft_isalpha
    modify the function so it can be better reused in other functions

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,28 +6,15 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 22:43:31 by mleonard          #+#    #+#             */
-/*   Updated: 2022/04/05 23:24:38 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/04/07 18:52:21 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define FALSE 0
-#define TRUE 1
-
 int	ft_isalpha(int c)
 {
-	if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'))
-		return (TRUE);
-	return (FALSE);
-}
-
-#include <stdio.h>
-#include <ctype.h>
-
-int	main(void)
-{
-	printf("Input: %c\nEsperado: %d\nRetornado: %d\n", 'a', 1, ft_isalpha('a'));
-	printf("\nInput: %c\nEsperado: %d\nRetornado: %d\n", 'b', 1, ft_isalpha('b'));
-	printf("\nInput: %c\nEsperado: %d\nRetornado: %d\n", '\\', 0, ft_isalpha('\\'));
-	printf("\nInput: %c\nEsperado: %d\nRetornado: %d\n", '?', 0, ft_isalpha('?'));
-	printf("\nInput: %c\nEsperado: %d\nRetornado: %d\n", '.', 0, ft_isalpha('.'));
+	if ('a' <= c && c <= 'z')
+		return (1);
+	if ('A' <= c && c <= 'Z')
+		return (2);
+	return (0);
 }
