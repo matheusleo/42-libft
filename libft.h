636684033131747fd55b34d6ba79853b63675e37
matheusleo <matheusleotrab@gmail.com>
2022-04-07 20:12:07 -0300

    add header file
    add the header file that will be required by the lib - libft.h

---
+++ libft.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/06 18:50:42 by mleonard          #+#    #+#             */
+/*   Updated: 2022/04/07 20:05:06 by mleonard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# define FALSE 0
+# define TRUE 1
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isprint(int c);
+int	ft_isascii(int c);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+
+
+#endif
