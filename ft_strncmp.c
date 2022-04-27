7936793a06621a33f1434007caa79a8ba7053791
matheusleo <matheusleotrab@gmail.com>
2022-04-26 22:08:30 -0300

    libc functions
    refactor the strncmp function to a better solution that is more legible

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 20:22:10 by mleonard          #+#    #+#             */
-/*   Updated: 2022/04/26 20:29:53 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/04/26 22:05:41 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	size_t	index;
+
+	index = 0;
 	if (!n)
 		return (0);
-	while ((n - 1) && *s1 && *s2 && (*s1 == *s2))
-	{
-		s1++;
-		s2++;
-		n--;
-	}
-	return ((unsigned char)*s1 - (unsigned char)*s2);
+	while (index < (n - 1) && s1[index] && s2[index] && s1[index] == s2[index])
+		index++;
+	return ((unsigned char)s1[index] - (unsigned char)s2[index]);
 }
